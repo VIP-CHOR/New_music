30629e3f43586f2336acd87fc70621a90a6a1615
VIP-CHOR <vp1576297@gmail.com>
2025-10-31 12:48:25 +0530

    Dilchor

--- BrandrdXMusic/utils/inline/play.py
+++ BrandrdXMusic/utils/inline/play.py
@@ -76,7 +76,7 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
             InlineKeyboardButton(text="🥂  ᴀᴅᴅ ᴍᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🥂", url=f"https://t.me/Amitamusicbot?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users"),
         InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"), 
         ],
-    
+    ]
     return buttons
 
 
